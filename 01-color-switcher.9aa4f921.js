07386e930343d5d0a971d2f48d6d1e90f1b3cbb5
Wolbu <94459033+Wolbu@users.noreply.github.com>
2022-06-22 11:52:50 +0000

    Deploying to gh-pages from @ Wolbu/goit-js-hw-09@afc4ec3ae8327e534e8169cf23578ef27e0bb94f 🚀

---
+++ 01-color-switcher.9aa4f921.js
@@ -0,0 +1,2 @@
+!function(){var t,o=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");function c(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),console.log("Start")}e.addEventListener("click",(function(){t=setInterval(c,1e3)})),n.addEventListener("click",(function(){clearInterval(t),console.log("stop")}))}();
+//# sourceMappingURL=01-color-switcher.9aa4f921.js.map
---
+++ 01-color-switcher.9dd6422c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),o=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n;function c(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,console.log("Start")}o.addEventListener("click",(function(){n=setInterval(c,1e3)})),e.addEventListener("click",(function(){clearInterval(n),console.log("stop")}));
+//# sourceMappingURL=01-color-switcher.9dd6422c.js.map
