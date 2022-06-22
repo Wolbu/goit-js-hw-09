afc4ec3ae8327e534e8169cf23578ef27e0bb94f
Wolbu <94459033+Wolbu@users.noreply.github.com>
2022-06-22 14:52:19 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', onStopButtonClick);
+
+let timerId;
+
+function startColorChange() {
+  timerId = setInterval(onStartButtonClick, 1000);
+}
+
+function onStartButtonClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  console.log('Start');
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  console.log('stop');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
