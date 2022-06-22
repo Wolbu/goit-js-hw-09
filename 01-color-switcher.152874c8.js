fc2e8c7f3de51e845c8f2de414773c58e47fac31
Wolbu <94459033+Wolbu@users.noreply.github.com>
2022-06-22 20:57:43 +0000

    Deploying to gh-pages from @ Wolbu/goit-js-hw-09@b6b1c16d6640cd276ef408e0d477a1370213d580 🚀

---
+++ 01-color-switcher.152874c8.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("body"),n=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");function a(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}n.addEventListener("click",(function(){t=setInterval(a,1e3),n.disabled=!0,o.disabled=!1})),o.addEventListener("click",(function(){clearInterval(t),n.disabled=!1,o.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.152874c8.js.map
